c2b7a53b7d0937f1a1de5deb57a284f4fb0ec6ff
alexanderdumas <55784337+alexanderdumas@users.noreply.github.com>
2023-12-21 11:34:29 +0800

    Create mpp.h

---
+++ mpp.h
@@ -0,0 +1,108 @@
+#ifndef _RKMPP_H_
+#define _RKMPP_H_
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <unistd.h>
+#include <errno.h>
+
+#include <string>
+#include <vector>
+
+#include "mpp_buffer.h"
+#include "mpp_frame.h"
+#include "mpp_packet.h"
+#include "rk_mpi.h"
+#include "vpu.h"
+#include "rk_type.h"
+
+#define ENV_BUF_SIZE_LINUX  1024
+
+
+#define MPP_ALIGN(x, a)         (((x)+(a)-1)&~((a)-1))
+
+typedef enum {
+    MPP_CODEC_DEC,  /**< decoder */
+    MPP_CODEC_ENC,  /**< encoder */
+} MppCodecType;
+
+typedef enum {
+    MPP_CODEC_SIMPLE,  /**< simple */
+    MPP_CODEC_ADVANCE,  /**< advance */
+} MppEasyType;
+
+
+class MppCodec {
+
+private:
+    //common 
+    RK_U32 _width;
+    RK_U32 _height;
+    RK_U32 _hor_stride;
+    RK_U32 _ver_stride;
+    RK_S32 _rc_mode;
+    RK_S32 _frame_num;
+    MppFrameFormat _format;
+    MppCodingType _type;
+    RK_U32 _need_split;
+    MppEasyType _easy_type;
+
+    size_t _header_size;
+    size_t _frame_size;
+    size_t _mdinfo_size;
+
+
+    //encode param
+    MppEncCfg _enc_cfg;
+
+    //decode param
+    MppDecCfg _dec_cfg;
+
+    //all need
+    MppCtx _ctx;
+    MppApi *_mpi;
+
+    MppBufferGroup _buf_grp;
+    MppBuffer _frm_buf;
+    MppBuffer _pkt_buf;
+    MppBuffer _mdi_buf;
+
+    MppPacket _packet;
+    MppFrame _frame;
+
+    RK_U8 *_base;
+    MppTask _task;
+
+    RK_S32 EncDefaultStride(RK_U32 width, MppFrameFormat fmt);
+    size_t GetFrameSize(MppFrameFormat format, RK_U32 hor_stride, RK_U32 ver_stride);
+    size_t GetMediaSize(MppCodingType code_type, RK_U32 hor_stride, RK_U32 ver_stride);
+    size_t GetHeadSize(MppFrameFormat format, RK_U32 width, RK_U32 height);
+    MPP_RET EncSetCfg();
+    MPP_RET EncReadImg(void *buf,void *input,size_t len, RK_U32 width, RK_U32 height,
+               RK_U32 hor_stride, RK_U32 ver_stride, MppFrameFormat format);
+    MPP_RET DecReadImg(void *buf,void *input,size_t len);
+    MPP_RET DumpToFrame(MppFrame frame,std::vector<unsigned char> & output);
+public:
+    MppCodec();
+    ~MppCodec();
+
+    MppCodec(const MppCodec &) = delete;
+    MppCodec(MppCodec&&) = delete;
+
+    MppCodec& operator=(const MppCodec &) = delete;
+    MppCodec& operator=(MppCodec&&) = delete;
+
+    void Init(MppCodecType codec_type, RK_U32 width, RK_U32 height,
+            MppFrameFormat format,  MppCodingType code_type,
+            RK_S32 need_split = 0, MppEasyType easy_type = MPP_CODEC_SIMPLE );
+    
+    MPP_RET Decode(void *input, size_t len, std::vector<unsigned char> & output);
+    MPP_RET Encode(void *input, size_t len, std::vector<unsigned char> & output);
+
+    MPP_RET ReSet();
+};
+
+
+
+#endif
